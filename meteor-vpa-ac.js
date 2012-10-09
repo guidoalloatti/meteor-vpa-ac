8a516075b427ed7f8ea6673db10bbb0f3fad52db
Bruno Bonamin <bbonamin@velocitypartners.net>
2012-10-09 12:26:51 -0300

    Initial implementation, people are being added correctly at startup!

--- meteor-vpa-ac.js
+++ meteor-vpa-ac.js
@@ -5,12 +5,6 @@ if (Meteor.isClient) {
     return Users.find({});
   }
 
-  Template.hello.greeting = function () {
-    return "Welcome to meteor-vpa-ac.";
-  };
-
-
-
   Template.user_list.events({
     'click .check' : function () {
       // template data, if any, is available in 'this'
@@ -24,7 +18,41 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+    Meteor.startup(function () {
+      if(Users.find().count() === 0) {
+        people = [{name:"JuanCa", aire: true},
+          {name:"Diegote", aire: true},
+          {name:"DaniM", aire: true},
+          {name:"NicoH", aire: true},
+          {name:"Eve", aire: true},
+          {name:"Gisela", aire: true},
+          {name:"Paula", aire: true},
+          {name:"DaniR", aire: true},
+          {name:"MartinL", aire: true},
+          {name:"Juan Manuel", aire: true},
+          {name:"Silvina", aire: true},
+          {name:"LeoT", aire: true},
+          {name:"Guido", aire: true},
+          {name:"Tate", aire: true},
+          {name:"Andy", aire: true},
+          {name:"Cacu", aire: true},
+          {name:"Mati", aire: true},
+          {name:"Pato", aire: true},
+          {name:"Rodri", aire: true},
+          {name:"Gonzo", aire: true},
+          {name:"Leo E", aire: true},
+          {name:"NicoRT", aire: true},
+          {name:"Mati C", aire: true},
+          {name:"March", aire: true},
+          {name:"NaN", aire: true},
+          {name:"Seba", aire: true},
+          {name:"Bruno", aire: true}
+          ];
+      
+        for( i=0; i< people.length; i++){
+          Users.insert({name: people[i].name, aire: true });
+          console.log('User fill' + i);
+        }
+      }
   });
-}
+};
